56ec7d422a2dde790cf93154a0d88ca7c98694a5
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-17 21:09:17 +0100

    add otlp expoter

--- otlp-exporter-instrumentation.js
+++ otlp-exporter-instrumentation.js
@@ -1,37 +1,32 @@
 /*instrumentation.js*/
+// Require dependencies
 const opentelemetry = require('@opentelemetry/sdk-node');
 const {
-    getNodeAutoInstrumentations,
-  } = require('@opentelemetry/auto-instrumentations-node');
-const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');  
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-proto');
+const {
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new opentelemetry.NodeSDK({
-    traceExporter: new ZipkinExporter({}),
-    instrumentations: [getNodeAutoInstrumentations()],
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: '<your-otlp-endpoint>/v1/traces',
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1, // an optional limit on pending requests
+    }),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start();
-// const {
-//   OTLPTraceExporter,
-// } = require('@opentelemetry/exporter-trace-otlp-proto');
-// const {
-//   OTLPMetricExporter,
-// } = require('@opentelemetry/exporter-metrics-otlp-proto');
-// const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-
-// const sdk = new opentelemetry.NodeSDK({
-//   traceExporter: new OTLPTraceExporter({
-//     // optional - default url is http://localhost:4318/v1/traces
-//     url: '<your-otlp-endpoint>/v1/traces',
-//     // optional - collection of custom headers to be sent with each request, empty by default
-//     headers: {},
-//   }),
-//   metricReader: new PeriodicExportingMetricReader({
-//     exporter: new OTLPMetricExporter({
-//       url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-//       headers: {}, // an optional object containing custom headers to be sent with each request
-//       concurrencyLimit: 1, // an optional limit on pending requests
-//     }),
-//   }),
-//   instrumentations: [getNodeAutoInstrumentations()],
-// });
