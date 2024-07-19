298894956574119dca0d6ee0920bcc062c0fc895
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-19 11:06:33 +0100

    add new open telemetry docker compose config

---
+++ opentelemetry-collector-dev-setup/zipkin-instrumentation.js
@@ -0,0 +1,39 @@
+/*zipkin-instrumentation.js*/
+// Require dependencies
+const opentelemetry = require('@opentelemetry/sdk-node');
+const {
+    getNodeAutoInstrumentations,
+  } = require('@opentelemetry/auto-instrumentations-node');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');  
+const { Resource } = require('@opentelemetry/resources');
+const {
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_VERSION,
+} = require('@opentelemetry/semantic-conventions');
+const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+
+// Custom Span Processor to filter out non-error spans
+class ErrorSpanProcessor extends BatchSpanProcessor {
+  onEnd(span) {
+    if (span.status.code !== 2) { // 2 corresponds to the status code for ERROR
+      return;
+    }
+    super.onEnd(span);
+  }
+}
+const traceExporter = new ZipkinExporter({
+  headers: {},
+})
+const errorSpanProcessor = new ErrorSpanProcessor(traceExporter);
+
+const sdk = new opentelemetry.NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'dice-server',
+    [SEMRESATTRS_SERVICE_VERSION]: '0.1.0',
+  }),
+  spanProcessor: errorSpanProcessor,
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
