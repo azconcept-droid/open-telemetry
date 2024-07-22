42445d812a1f69d9108994b2aaa74dc887a1831b
Yahaya Azeez <azconcept2016@gmail.com>
2024-07-22 15:50:22 +0100

    add span function

--- nextjs-blog/instrumentation.js
+++ nextjs-blog/instrumentation.js
@@ -1,5 +1,18 @@
 import { registerOTel } from '@vercel/otel'
- 
+import { trace } from '@opentelemetry/api'
+
 export function register() {
-  registerOTel({ serviceName: 'next-app' })
+    registerOTel({ serviceName: 'next-app' })
+  }  
+ 
+export async function nextjsApp() {
+  return await trace
+    .getTracer('nextjs-example')
+    .startActiveSpan('nextjsApp', async (span) => {
+      try {
+        return await getValue()
+      } finally {
+        span.end()
+      }
+    })
 }
